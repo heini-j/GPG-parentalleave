de3b1a038ebeaa00357b7254ba9d8420f1c02b2e
Heini Järviö <heinimjarv@gmail.com>
2025-03-07 17:39:52 +0100

    sample searching, creating plots

--- data-exploration.R
+++ data-exploration.R
@@ -1,7 +1,8 @@
 library(dplyr)
 library(readr)
 library(tidyr)
-
+library(ggplot2)
+library(ggrepel)
 
 # Load the data
 
@@ -57,15 +58,76 @@ df_gwg_common |>
 
 # oecd has data from 2005 to 2023
 
-# Discovering paternity leave lenghts ----------------------------------------
+# filtering the datasets to only include the years that are covered by both datasets
+
+df_parentalleave_common <- df_parentalleave_common |>
+  filter(year >= 2005 & year <= 2023)
+
+# Discovering paternity leave lengths ----------------------------------------
+
+# Plotting the leave lengths in the 44 countries to see how they have changed over time
+
+df_filtered <- df_parentalleave_common |> 
+  group_by(country) |> 
+  filter(any(year == 2020 & paternityleave_length >= 14)) |>
+  filter(year >= 2008 & year <= 2020)
+
+filtered_countries <- unique(df_filtered$country)
+
+# 18 countriess left after filtering
+
+selection_countries <- df_filtered |>
+  ggplot(aes(x = year, y = paternityleave_length, color = country)) +
+  geom_line() +
+  facet_wrap(~country) +
+  geom_text_repel(data = df_filtered |> filter(year == 2020), 
+                  aes(label = country), 
+                  hjust = 0, # Position labels slightly to the right
+                  nudge_x = 0.2, # Move labels slightly away from the last point
+                  direction = "y", # Avoid overlapping
+                  size = 2,
+                  max.overlaps = 15) +
+  scale_x_continuous(breaks = seq(2005, 2020, by = 1)) +
+  labs(title = "Paternity leave lengths in 18 countries",
+       x = "Year",
+       y = "Length of paternity leave (days)",
+       color = "Country") +
+  theme_minimal()+
+  theme(legend.position = "none")
+
+ggsave("plots/selected.png", selection_countries, width = 20, height = 20)
+
+# Here, we can see that for the purpose of this study, Australia, Bulgaria, Cyprus,
+# Finland, Ireland, Lithuania, Luxembourg, Poland, Portugal, Slovenia and Spain
+# had an increase of at least 14 days in the relevant time period
+
+# Of these, the increase was from 0 to 14 days in Australia, Bulgaria, Cyprus, Ireland, Lithuania (to 30 days),
+# Poland
+
+# Belgium, Denmark, Estonia, Iceland, Romania, Sweden and the UK had no changes to the paternity leave (>= 14 days) in this time period
+
+# Leave length was 0 throughout the time period in Austria, Canada, Costa Rica, Croatia, Germany,
+# Israel, Japan, New Zealand, Norway, Slovak Republic, Switzerland and the US
+
+# leave length was less than 14 d in 2020 in Argentina, Brazil, Chile, Colombia, Czechia, France (decrease from 15), Greece,
+# HUngary, Italy, Latvia, Malta, Maxico, Netherlands & Türkiye
 
 # Cut-off at 5 years prior and after 
 
-# Which countries had paternity leave of at least 14 days in 2018 
+# Which countries had a change in the length of paternity leave between 2009 and 2019?
+
+df_parentalleave_common |> 
+  filter(year %in% c(2009, 2019)) |> # Keep only relevant years
+  group_by(country) |> # Group by country to check both years
+  filter(any(year == 2019 & paternityleave_length >= 7) & 
+           any(year == 2009 & paternityleave_length < 7)) |> 
+  summarise(n = n_distinct(country))
+
 
 df_parentalleave_common |>
-  filter(year == 2018 & paternityleave_length >= 14)|>
-  summarise(n())
+  filter(year == 2009 & paternityleave_length >= 14) |>
+  group_by(country) |>
+  summarise(n = n_distinct(country))
 
 # 17 countries
 
