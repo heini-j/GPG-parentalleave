119e665d98d4b799eeb5f23600ce9ce399b64752
Heini Järviö <heinimjarv@gmail.com>
2025-03-17 20:19:35 +0100

    added control variables

--- did_preparations.R
+++ did_preparations.R
@@ -1,6 +1,8 @@
 library(tidyr)
 library(did)
 library(readxl)
+library(ggplot2)
+library(ggrepel)
 
 # Reading the data to R -------------------------------------------------------
 
@@ -22,6 +24,9 @@ View(df_did)
 df_did <- df_did |>
   mutate(treatment = ifelse(country %in% c("Australia", "Canada", "South Korea", "Germany", "Japan", "United Kingdom"), 1, 0))
 
+df_did <- df_did |>
+  mutate(treatment = as.factor(treatment))
+
 # Adding control variables to the data -----------------------------------------
 
 # generic gender equality index
@@ -30,6 +35,8 @@ equality <- read_excel("WBL2024-1-0-Historical-Panel-Data.xlsx",
            sheet = "WBL Panel 2024",
            range = cell_cols("A:G"),
            col_names = TRUE)
+
+# transforming the data to match with the main dataframe
            
 equality <- equality |>
   rename(country = "Economy",
@@ -42,15 +49,71 @@ equality <- equality |>
 
 equality$year <- as.numeric(equality$year)
 
+# Adding to the main dataframe
 
 df_did <- df_did |>
   left_join(equality, by = c("country", "year"))
            
-# GDP per capita
+# GDP per capita & Gini index
 
-# Gini index
+gdp <- read_csv("oecd_gdp.csv")
+
+View(gdp)
+
+gdp <- gdp |> 
+  select("Reference area", "TIME_PERIOD", "OBS_VALUE") |>
+  rename(gdp = "OBS_VALUE")
+
+gini <- read_csv("oecd_gini.csv")
+
+View(gini)
+
+gini <- gini |> 
+  select("Reference area", "TIME_PERIOD", "OBS_VALUE") |>
+  rename(gini = "OBS_VALUE")
+
+gdp <- gdp |>
+  left_join(gini, by = c("Reference area", "TIME_PERIOD")) |>
+  rename(country = "Reference area",
+         year = "TIME_PERIOD")
+
+# Adding the values to the main dataframe
+
+df_did <- df_did |>
+  left_join(gdp, by = c("country", "year"))
 
 # Comparing treatment and control conditions -----------------------------------
+
+# testing parallel trends assumption
+
+summary_did <- df_did |> 
+  filter(year < 2012) |>
+  group_by(treatment) |>
+  summarize(
+    mean_gdp = mean(gdp, na.rm = TRUE),
+    mean_gender_equality = mean(equality_index, na.rm = TRUE),
+    mean_gini = mean(gini, na.rm = TRUE),
+    mean_wage_gap = mean(gwg_median, na.rm = TRUE),
+    mean_d1 = mean(gwg_d1, na.rm = TRUE),
+    mean_d9 = mean(gwg_d9, na.rm = TRUE)
+  )
+
+View(summary_did)
+
+# plotting the gwg median values per treatment group and year
+
+df_did |>
+  filter(year < 2012) |>
+  ggplot(aes(x = year, y = gwg_median, color = treatment, group = country)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE, linewidth = 0.7) +
+  geom_text_repel(
+    data = df_did |> filter(year == 2011),  # Label only the last year
+    aes(label = country),
+    hjust = 1, nudge_x = 0.5, size = 3
+  ) +
+  labs(title = "X") +
+  theme_minimal()
 
 
 
