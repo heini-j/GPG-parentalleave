74b3edd001fd06cd59529726eec83678664b176a
Heini Järviö <heinimjarv@gmail.com>
2025-03-26 18:49:27 +0100

    fixed problems in df creation

--- combining_dfs.R
+++ combining_dfs.R
@@ -42,6 +42,7 @@ summarise(country_list, n())
 df_parentalleave_common <- df_parentalleave |>
   filter(country %in% country_list$country)
 
+
 df_gwg_common <- df_gwg |>
   filter(country %in% country_list$country)
 
@@ -72,10 +73,13 @@ df_gwg_common <- df_gwg_common |>
 gwg_wide <- df_gwg_common |>
   pivot_wider(names_from = gwg_type, values_from = gwg)
 
+
 # merging the dataframes
 
 df_combined <- df_parentalleave_common |>
   left_join(gwg_wide, by = c("country", "year"))
+
+
 
 View(df_combined)
 
@@ -97,6 +101,99 @@ df_combined <- df_combined |>
    gwg_median = "MEDIAN",
    gwg_d1 = "D1",
    gwg_d9 = "D9")
+
+
+# Adding coveriates -----------------------------------------------------------
+
+# general gender equality index
+
+equality <- read_excel("WBL2024-1-0-Historical-Panel-Data.xlsx", 
+                       sheet = "WBL Panel 2024",
+                       range = cell_cols("A:G"),
+                       col_names = TRUE)
+
+# transforming the data to match with the main dataframe
+
+equality <- equality |>
+  rename(country = "Economy",
+         year = "Report Year",
+         equality_index = "WBL INDEX") |>
+  select(country, year, equality_index) |>
+  mutate(country = ifelse(country == "Korea, Rep.", "South Korea", country))
+
+equality$year <- as.numeric(equality$year)
+
+# Adding to the main dataframe
+
+df_combined <- df_combined |>
+  left_join(equality, by = c("country", "year"))
+
+df_combined |>
+  count(country, year) |>
+  filter(n > 1)
+
+# GDP per capita & Gini index
+
+gdp <- read_csv("oecd_gdp.csv")
+
+View(gdp)
+
+gdp <- gdp |> 
+  select("Reference area", "TIME_PERIOD", "OBS_VALUE", "Transaction") |>
+  filter(Transaction == "Gross domestic product") |>
+  rename(gdp = "OBS_VALUE")
+
+gini <- read_csv("oecd_gini.csv")
+
+View(gini)
+
+gini <- gini |> 
+  select("Reference area", "TIME_PERIOD", "OBS_VALUE") |>
+  rename(gini = "OBS_VALUE")
+
+gdp <- gdp |>
+  left_join(gini, by = c("Reference area", "TIME_PERIOD")) |>
+  rename(country = "Reference area",
+         year = "TIME_PERIOD")
+
+gdp <- gdp |>
+  mutate(country = ifelse(country == "Korea", "South Korea", country))
+
+# Adding the values to the main dataframe
+
+df_combined <- df_combined |>
+  left_join(gdp, by = c("country", "year"))
+
+
+df_combined |>
+  count(country, year) |>
+  filter(n > 1)
+
+# women's labour force participation
+
+lf_participation <- read_csv("Labor force participation rate, total (% of total population ages 15-64) (modeled ILO estimate).csv")
+
+
+unique(lf_participation$"Disaggregation")
+
+View(lf_participation)
+
+lf_participation <- lf_participation |>
+  select("Country Name", "Year", "Value", "Disaggregation") |>
+  filter(Disaggregation == "female, Modeled, 15+") |>
+  rename(country = "Country Name",
+         year = "Year",
+         lf_participation = "Value") |>
+  mutate(country = ifelse(country == "Korea, Rep.", "South Korea", country))
+
+# Adding to the main dataframe
+
+
+df_combined <- df_combined |>
+  left_join(lf_participation, by = c("country", "year"))
+
+
+View(df_combined)
 
 
 # Saving the combined data --------------------------------------------------
